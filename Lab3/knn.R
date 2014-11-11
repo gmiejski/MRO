9b894e6c6d8c3c7647f11eacb95db638c523bdaa
Grzegorz Miejski <grz.miejski@gmail.com>
2014-11-11 23:11:24 +0100

    KNN method implemented

--- Lab3/knn.R
+++ Lab3/knn.R
@@ -1,41 +1,67 @@
 library(class)
 
-data <- read.csv(file="breast-cancer-wisconsin.data",head=FALSE,sep=",")
+data <- read.csv(file="breast-cancer-wisconsin.data",head=TRUE,sep=",")
 data <- data[!apply(data,1,function(x)any('?' %in% c(x))),]
-names(data)
-colnames(data) <- c("id","clump thickness","cell size","cell shape","marginal adhesion","Single Epithelial Cell Size","Bare Nuclei","Bland Chromatin","Normal Nucleoli","Mitoses","class")
+dataSize = nrow(data)
+data <- data[sample(dataSize),]
+data.orig <- data
 
-splitRate = 4
-dataInEachPart = as.integer(nrow(data) / splitRate)
+data <- data.orig
+data <- scale(data[,c(2:6, 8:10)])
+
+ncol(data)
+colnames(data) <- c("clump thickness","cell size","cell shape",
+                    "marginal adhesion","Single Epithelial Cell Size",
+                    "Bland Chromatin","Normal Nucleoli","Mitoses") #,"class")
+colnames(data)
+
+# splitRate = 4
+# dataInEachPart = ceiling(nrow(data) / splitRate)
 
 
 # for ( x in 0:splitRate-1) {
 #   print ( x*dataInEachPart)  
-  x = 0
-  testSetIndexes = c((x*dataInEachPart):((x+1)*(dataInEachPart -1)))
-  trainingSet = data[-c(testSetIndexes),]
-  clasifier = factor(c(trainingSet[,11]))
-  trainingSet = trainingSet[,1:10]
+getSuccessRatio <- function (splitRate, knn_k ) {
+#   x = 0
   
-  testSet = data[c(testSetIndexes),]
+  print(splitRate-1)
   
-  data.knn <- knn(trainingSet, testSet[,1:10], clasifier, k = 1, prob=TRUE)
-  data.knn  
-  data.knn.diff = testSet[,11]-(as.integer(data.knn[1:(dataInEachPart-1)])*2)
-  data.knn.diff.ok = length(which(data.knn.diff == 0))
-  data.knn.diff.ok = length(data.knn.diff[data.knn.diff == 0])
-  data.knn.diff.bad = length(which(data.knn.diff != 0))
-  
-# }
+  dataInEachPart = ceiling(nrow(data) / splitRate)
+  successRates = rep(NA, splitRate)
+  for ( x in 1:splitRate-1) {  
+    testSetIndexes = c((x*dataInEachPart):((x+1)*(dataInEachPart -1)))
+    trainingSet = data[-c(testSetIndexes),]
+    clasifier = factor(c(data.orig[-c(testSetIndexes),11]))
+    testSet = data[c(testSetIndexes),]
+    
+    data.knn <- knn(trainingSet, testSet, clasifier, k = knn_k, prob=TRUE)
+    data.knn  
+    data.knn.diff = data.orig[c(testSetIndexes),11]-(as.integer(data.knn[1:(dataInEachPart-1)])*2)
+    data.knn.diff.ok = length(which(data.knn.diff == 0))
+    data.knn.diff.ok = length(data.knn.diff[data.knn.diff == 0])
+    data.knn.diff.bad = length(which(data.knn.diff != 0))
+    print(x)
+    print(data.knn.diff.ok)
+    print(data.knn.diff.bad)
+    print(data.knn.diff.ok / (data.knn.diff.ok+data.knn.diff.bad))
+    successRates[x+1] <- data.knn.diff.ok / (data.knn.diff.ok+data.knn.diff.bad)
+  }
 
-data.knn[1:(dataInEachPart-1)]
+  sum(successRates) / length(successRates)
+  
+}
+
+successRatios = getSuccessRatio(4,1)
+
+
+# data.knn[1:(dataInEachPart-1)]
 # train <- rbind(iris3[1:25,,1], iris3[1:25,,2], iris3[1:25,,3])
 # test <- rbind(iris3[26:50,,1], iris3[26:50,,2], iris3[26:50,,3])
 # cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
 # knn(train, test, cl, k = 3, prob=TRUE)
-attributes(.Last.value)
+# attributes(.Last.value)
 
 # cut data examples not having all the information
-data[!apply(data,1,function(x)any('?' %in% c(x))),]
+# data[!apply(data,1,function(x)any('?' %in% c(x))),]
 
-trainingSet
+# trainingSet
